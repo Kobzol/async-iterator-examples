1360a92c45479512131ad81c7f673e3493e5e2c1
Jakub Beránek <berykubik@gmail.com>
2024-01-04 19:28:12 +0100

    Remove `VecDeque`

--- github-api-pagination/src/main.rs
+++ github-api-pagination/src/main.rs
@@ -15,37 +15,37 @@ use pin_project_lite::pin_project;
 struct RepoIterAfit<'a> {
     client: &'a Octocrab,
     page: Option<u32>,
-    repos: VecDeque<Repository>
+    repos: std::vec::IntoIter<Repository>
 }
 
 /// The ergonomics here are not great, as the splitting of batches into individual items
-/// requires me to manually store the state in `RepoIterAfit`. I also have to copy the data
-/// from a `Vec` into `VecDeque` to enable efficient iteration in original order.
+/// requires me to manually store the state in `RepoIterAfit`.
 impl<'a> AfitAsyncIter for RepoIterAfit<'a> {
     type Item = Repository;
 
     async fn next(&mut self) -> Option<Self::Item> {
         loop {
-            if self.repos.is_empty() {
-                if let Some(page) = self.page {
-                    let repos = self.client.orgs("rust-lang").list_repos()
-                        .per_page(100)
-                        .page(page)
-                        .send()
-                        .await
-                        .unwrap();
-                    if repos.next.is_none() {
-                        self.page = None;
-                    } else {
-                        self.page = Some(page + 1);
-                    }
-                    self.repos = repos.into_iter().collect();
-                    continue;
-                } else {
-                    return None;
-                }
+            let next = self.repos.next();
+            if let Some(next) = next {
+                return Some(next);
             }
-            return Some(self.repos.pop_front().unwrap());
+            if let Some(page) = self.page {
+                let repos = self.client.orgs("rust-lang").list_repos()
+                    .per_page(100)
+                    .page(page)
+                    .send()
+                    .await
+                    .unwrap();
+                if repos.next.is_none() {
+                    self.page = None;
+                } else {
+                    self.page = Some(page + 1);
+                }
+                self.repos = repos.into_iter();
+                continue;
+            } else {
+                return None;
+            }
         }
     }
 }
@@ -70,20 +70,20 @@ async fn iterate_repos_async_gen(client: &Octocrab) -> u32 {
     let mut iter = async gen {
         let mut page = 0u32;
         loop {
-            let mut repos = client.orgs("rust-lang").list_repos()
-                .per_page(100)
-                .page(page)
-                .send()
-                .await
-                .unwrap();
-            for repo in repos.take_items() {
-                yield repo;
-            }
-            if repos.next.is_none() {
-                break;
-            } else {
-                page += 1;
-            }
+        let mut repos = client.orgs("rust-lang").list_repos()
+        .per_page(100)
+        .page(page)
+        .send()
+        .await
+        .unwrap();
+        for repo in repos.take_items() {
+        yield repo;
+        }
+        if repos.next.is_none() {
+        break;
+        } else {
+        page += 1;
+        }
         }
     };
 
@@ -127,8 +127,8 @@ impl<'a> PollNextAsyncIter for RepoIterPoll<'a> {
 async fn main() -> anyhow::Result<()> {
     let client = octocrab::OctocrabBuilder::default()
         .build()?;
-    // let count = iterate_repos_afit(&client).await;
-    let count = iterate_repos_async_gen(&client).await;
+    let count = iterate_repos_afit(&client).await;
+    // let count = iterate_repos_async_gen(&client).await;
 
     println!("{count}");
 
