11bffc8b74f1e07cfb6570e808b5c26c0c4f4551
Jakub Beránek <jakub.beranek@vsb.cz>
2024-01-04 22:45:29 +0100

    Add incomplete `poll_next` version of the GH pagination API

--- github-api-pagination/src/main.rs
+++ github-api-pagination/src/main.rs
@@ -1,8 +1,10 @@
 #![feature(gen_blocks)]
 #![feature(async_iterator)]
 
+extern crate octocrab;
+
 use octocrab::models::Repository;
-use octocrab::Octocrab;
+use octocrab::{Octocrab, Page};
 use pin_project_lite::pin_project;
 use shared::{AfitAsyncIter, PollNextAsyncIter};
 use std::async_iter::AsyncIterator;
@@ -10,6 +12,7 @@ use std::collections::VecDeque;
 use std::future::{poll_fn, Future};
 use std::pin::{pin, Pin};
 use std::task::{Context, Poll};
+use octocrab::orgs::ListReposBuilder;
 
 // AFIT version
 struct RepoIterAfit<'a> {
@@ -99,33 +102,45 @@ async fn iterate_repos_async_gen(client: &Octocrab) -> u32 {
 }
 
 // poll version
-pin_project! {
-    struct RepoIterPoll<'a> {
-        client: &'a Octocrab,
-        #[pin]
-        future: Option<Pin<Box<dyn Future<Output=octocrab::Result<octocrab::Page<Repository>>>>>>,
-        page: Option<u32>,
-        repos: VecDeque<Repository>,
-    }
-}
+// Impossible due to octocrab using double lifetime?
+// If anyone knows how to write this manually, I'd be glad for help :)
 
-impl<'a> PollNextAsyncIter for RepoIterPoll<'a> {
-    type Item = Repository;
+// pin_project! {
+//     struct RepoIterPoll<'a> {
+//         client: &'a Octocrab,
+//         #[pin]
+//         future: Option<Box<dyn Future<Output=octocrab::Result<octocrab::Page<Repository>>> + 'a>>,
+//         page: Option<u32>,
+//         repos: std::vec::IntoIter<Repository>,
+//     }
+// }
 
-    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        let mut this = self.as_mut();
-        // match &this.future {
-        //     Some(fut) => {
-        //     }
-        //     None => {
-        //
-        //     }
-        // }
-
-        todo!();
-        Poll::Pending
-    }
-}
+// impl<'a> PollNextAsyncIter for RepoIterPoll<'a> {
+//     type Item = Repository;
+//
+//     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+//         let mut this = self.project();
+//         let client: &'a Octocrab = this.client;
+//
+//         if let Some(item) = this.repos.next() {
+//             return Poll::Ready(Some(item));
+//         }
+//         let Some(page) = this.page else { return Poll::Ready(None); };
+//
+//         if this.future.is_none() {
+//             let handler = client.orgs("rust-lang");
+//             let handler: ListReposBuilder<'a, '_> = handler.list_repos().per_page(100)
+//                 .page(*page);
+//             let handler: Box<dyn Future<Output=octocrab::Result<Page<Repository>>>> = Box::new(handler.send());
+//             this.future.as_mut().set(Some(handler));
+//         }
+//
+//         // this.future.as_pin_mut().unwrap().poll(cx);
+//
+//         todo!();
+//         Poll::Pending
+//     }
+// }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
